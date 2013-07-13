57ec7fbc65f0d45ab9ddf6d4201ca4348e3ee2af
cooldeez <cooldeezdesigns@gmail.com>
2013-07-13 15:19:19 +0530

    the base rails4 ruby 2 mysql bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheStation::Application.config.secret_key_base = 'ac472e8d9d2842cc45789b8c85b8bc9c7ab6daa3082af9798a3592469208ea661af953248d2ce8d9ec67ed8625d03e9d5e6ee67ef58ad03344665cd531eb1bab'
